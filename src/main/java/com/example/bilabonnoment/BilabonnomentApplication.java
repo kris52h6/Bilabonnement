1224359a439421c0cbe1202f8c96c962b0c554ed
kris52h6 <kris52h6@stud.kea.dk>
2022-05-02 13:26:08 +0200

    init

---
+++ src/main/java/com/example/bilabonnoment/BilabonnomentApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnoment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilabonnomentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilabonnomentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilabonnoment/BilabonnomentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnoment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilabonnomentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
