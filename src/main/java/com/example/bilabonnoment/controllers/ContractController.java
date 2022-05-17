2ddc21081f499bd5592cfd10b21271da36a1bfed
Oliver <oliv2058@stud.kea.dk>
2022-05-18 00:03:40 +0200

    removed unused imports

--- src/main/java/com/example/bilabonnoment/controllers/ContractController.java
+++ src/main/java/com/example/bilabonnoment/controllers/ContractController.java
@@ -1,6 +1,5 @@
 package com.example.bilabonnoment.controllers;
 
-import com.example.bilabonnoment.models.Contract;
 import org.springframework.stereotype.Controller;
 import com.example.bilabonnoment.repositories.ContractRepository;
 import org.springframework.ui.Model;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
-
-import java.sql.Date;
 
 @Controller
 public class ContractController {
