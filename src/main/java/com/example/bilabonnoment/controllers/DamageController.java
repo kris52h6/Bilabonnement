bfc81e694e61abf73266a3f1587470d9b2674236
OliverAndreasen <oliv2058@stud.kea.dk>
2022-05-18 11:04:38 +0200

    commit to pull

--- src/main/java/com/example/bilabonnoment/controllers/DamageController.java
+++ src/main/java/com/example/bilabonnoment/controllers/DamageController.java
@@ -37,7 +37,7 @@ public class DamageController {
     @PostMapping("/createDamage")
     public String createDamage(WebRequest dataFromForm) {
         damageRepository.createDamage(dataFromForm);
-        return "redirect:/allContracts";
+            return "redirect:/allContracts";
     }
 
     @GetMapping("/deleteDamage")
